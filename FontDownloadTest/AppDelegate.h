54fa15edbff599d9a3d55515d4aac3677eb330eb
Eden Li <eden@realtime.tw>
2016-06-30 13:49:20 +0800

    Initial Commit

---
+++ FontDownloadTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FontDownloadTest
+//
+//  Created by EdenLi on 2016/6/30.
+//  Copyright © 2016年 Darktt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FontDownloadTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FontDownloadTest
+//
+//  Created by EdenLi on 2016/6/30.
+//  Copyright © 2016年 Darktt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
